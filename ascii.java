8933686e098e761d3136d74663458c64d8555fa7
Nithin947 <gudapatinithin@gmail.com>
2024-01-23 17:33:49 +0530

    first message

---
+++ ascii.java
@@ -0,0 +1,18 @@
+class ASCII
+{
+	public static void main(String args[])
+	{
+		int num,a,count;
+		for(num=1;num<=255;num++)
+		{
+			a = 1;
+			count = 0;
+			while(a<=num)
+			{
+				if(count)
+				count++;
+				a++;
+			
+		}
+	}
+}
